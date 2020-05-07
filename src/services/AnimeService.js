f446db640b626d2bf7ca7fb9c976e5ecc77b7e22
AxlRicci <alexbricci@gmail.com>
2020-05-06 21:57:03 -0400

    created axios api client to call kitsu

---
+++ src/services/AnimeService.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'https://kitsu.io/api/edge/',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/vnd.api+json',
+    'Content-Type': 'application/vnd.api+json'
+  }
+})
+
+export default {
+  // add call methods here. below is sample of form.
+  getAnime() {
+    return apiClient.get()
+  }
+}
